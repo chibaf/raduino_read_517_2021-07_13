9a6c4383e2bfd374efb03d10f3fa4b0436635692
chibaf <chibaf@mac.com>
2021-07-13 02:31:28 +0900

    Add files via upload

---
+++ ard_serial_main.py
@@ -0,0 +1,11 @@
+import serial, sys
+import syslog
+import time
+from ard_serial_sub import ard_serial_sub
+
+port = sys.argv[1]
+speed = sys.argv[2]
+for i in range(0,10):
+  array=ard_serial_sub(port,speed)
+  print(array)
+exit()
